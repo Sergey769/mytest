d72bf03f6282ffccd0f14f17685b065ec3557951
Sergey <sergey@Event.(none)>
2012-11-12 20:35:13 +0400

    Initialize commit

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '105a407ed85345162e8016d118843c3329e214680acece6bdfd3908c901be7fe43653da150990699704321a556c4608191f0939c67b347bff025b41d3e223d99'
---
+++ mytest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytest::Application.config.secret_token = 'f4aee7fc0acd856ce87341596cdc7444081d5145490db30c387757f114c8f3ff804de7e6233dbcc407d36be80ee12a7a3681a238ab0b4faf408fc875b1ddb46a'
